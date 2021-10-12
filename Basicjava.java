bae2076e02bd22b21854bbfbaccc805b9e54fabc
candra <hidayatcandra110398@gmail.com>
2021-10-12 15:21:15 +0700

    first commit

---
+++ Basicjava.java
@@ -0,0 +1,9 @@
+public class BasicJava{
+    public static void main(String[] args) {
+    String firstName = "12341234" ; // 1
+    int number = 1234;
+    System.out.println(firstName + number);  
+    }
+}
+    
+
---
+++ Challeng.java
@@ -0,0 +1,20 @@
+public class Challeng {
+ public static void main(String[] args) {
+    // int Number =1234;
+    // System.out.println(firstName + Number);
+    String firstName = "candra ";
+    String lastName = "hidayat ";
+    String age = "22 ";
+    String address = "jl.mangga 3 kota padang ";
+    String phoneNumber = "089643418592 ";
+    String hobi = " joging ";
+    String makananKesukaan =" sate padang ";
+    System.out.println("first name : " + firstName);
+    System.out.println("last name : " + lastName);
+    System.out.println("Age : " + age);
+    System.out.println("address : " + address);
+    System.out.println("phoneNumber : " + phoneNumber);
+    System.out.println("hobi :" + hobi);
+    System.out.println("makanan kesukaan : " + makananKesukaan);
+  }
+}
