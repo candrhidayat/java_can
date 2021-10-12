ad9ee5f943d0d24a06487aa72ac30a121ecbdbee
candra <hidayatcandra110398@gmail.com>
2021-10-12 22:35:40 +0700

    https://github.com/candrhidayat/riyan123.git

---
+++ public class BasicJava {.java
@@ -0,0 +1,23 @@
+public class BasicJava {
+    String username;
+    final String lastName = "world";
+    public static void main(String[] args){
+        // leave comment in here
+        // int integerNunber;
+        // integerNumber;
+        // system.out.println(integerNumber);
+        // String firstName = "Ryan";
+        // String lastName = "Elvian";
+        // String thirdName = "Susandy";
+        // int numer = 1997;
+        // double dollar = 10.14;
+        // System.out.println(firstName + number);
+        System.out.println("first name :" + "Ryan");
+        System.out.println("last name :" + "Elvian");
+        System.out.println("age :" + "23 tahun");
+        System.out.println("adress :" + "bukittinggi,sumbar");
+        System.out.println("phone number :" + "085264609770");
+        System.out.println("hobby :" + "badminton");
+        System.out.println("favorite food :" + "randang");
+    }
+}
